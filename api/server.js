1b8c5e90ebebdbf56b6bf033c9dbfc37e8e0a6f1
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-18 17:15:27 -0600

    all test passing

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 // build your server here and require it from index.js
 const express = require("express")
-// const ProjectRouter = require("./project/router")
-// const ResourceRouter = require("./resource/router")
-// const TaskRouter = require("./task/router")
+const ProjectRouter = require("./project/router")
+const ResourceRouter = require("./resource/router")
+const TaskRouter = require("./task/router")
 const server = express()
 
 server.use(express.json())
-// server.use("/api/projects", ProjectRouter)
-// server.use("/api/resources", ResourceRouter)
-// server.use("/api/tasks", TaskRouter)
+server.use("/api/projects", ProjectRouter)
+server.use("/api/resources", ResourceRouter)
+server.use("/api/tasks", TaskRouter)
 
 // eslint-disable-next-line no-unused-vars
 server.use((err, req, res, next) => {
