2b1112c43336e2a27add9b1c80af29f68fc624c4
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-18 16:21:31 -0600

    Added migration

--- api/server.js
+++ api/server.js
@@ -1,15 +1,16 @@
 // build your server here and require it from index.js
 const express = require("express")
-const ProjectRouter = require("./project/router")
-const ResourceRouter = require("./resource/router")
-const TaskRouter = require("./task/router")
+// const ProjectRouter = require("./project/router")
+// const ResourceRouter = require("./resource/router")
+// const TaskRouter = require("./task/router")
 const server = express()
 
 server.use(express.json())
-server.use("/api/projects", ProjectRouter)
-server.use("/api/resources", ResourceRouter)
-server.use("/api/tasks", TaskRouter)
+// server.use("/api/projects", ProjectRouter)
+// server.use("/api/resources", ResourceRouter)
+// server.use("/api/tasks", TaskRouter)
 
+// eslint-disable-next-line no-unused-vars
 server.use((err, req, res, next) => {
   res.status(500).json({ message: err.message, stack: err.stack })
 })
